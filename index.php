045f07472103d120f93dadeaf6c4e743dc23fe9f
jms007ll <59643568@qq.com>
2015-12-09 16:47:02 +0800

    三个板块初始化

---
+++ index.php
@@ -0,0 +1,19 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<title>北京金熙盛印刷</title>
+<link href="layout.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="container">
+  <div id="header">
+	<a class='ss'>啊啊啊啊啊啊啊</a>
+  </div>
+  <div id="mainContent">
+    <div id="sidebar">北京金熙盛印刷1</div>
+    <div id="sidebar2">北京金熙盛印刷2</div>
+    <div id="sidebar3">北京金熙盛印刷3</div>
+</div>
+</div>
+</body>
+</html>
---
+++ message.php
@@ -0,0 +1,25 @@
+<link href="base.css" rel="stylesheet" media="screen">
+<section id="product">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4 service-box solmail" onClick="SolUtil.moveToSvc('mail', 'ko');">
+        <div class="service_desc">
+          <h2>北京金熙盛印刷</h2>
+          <p></p>
+        </div>
+      </div>
+      <div class="col-sm-4 service-box solcalendar" onClick="SolUtil.moveToSvc('calendar', 'ko');">
+        <div class="service_desc">
+          <h2>金熙盛印刷</h2>
+          <p></p>
+        </div>
+      </div>
+      <div class="col-sm-4 service-box solgroup" onClick="SolUtil.moveToSvc('group', 'ko');">
+        <div class="service_desc">
+          <h2>金熙盛</h2>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
